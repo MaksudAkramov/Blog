668342286065ee809bf8dd5f9b5c00125676884a
MaksudAkramov <makramov44@gmail.com>
2021-11-18 15:26:49 +0500

    added profile photo directory so that it show profile's photo after comment
    #9

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', PostListView.as_view(), name='home'),
     path('<int:pk>', PostDetailView.as_view(), name='post_detail'),
     path('create/', add_post, name='create'),
-    path('<int:post_id>/comment', comment_post, name='comment'),
+    path('<post_id>/comment', comment_post, name='comment'),
     path('aboutus', AboutUsView.as_view(), name='aboutus'),
 ]
