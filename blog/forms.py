e15591753f93f29a5a8bff457d25b29abee4fe43
MaksudAkramov <makramov44@gmail.com>
2021-11-18 14:25:06 +0500

    new feature: add post, if post has no photo, default photo is also added
    #7

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,13 @@
 from django import forms
 
+from blog.models import Post
+
 class CommentForm(forms.Form):
     comment = forms.CharField(widget=forms.Textarea(attrs={'rows': 1, 'cols': 100}))   
+
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'author']   
